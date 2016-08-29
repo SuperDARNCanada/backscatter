015236495013f93556c2e5648c108f739635542c
sd-usask <kevin.krieger@usask.ca>
2016-08-29 16:29:20 -0600

    using python2 mock

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@
 # sys.path.insert(0, os.path.abspath('.'))
 #mock modules for c extensions
 import sys
-from unittest.mock import MagicMock
+from mock import Mock as MagicMock
 
 class Mock(MagicMock):
     @classmethod
