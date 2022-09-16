16ca49f6133183d4b383d4d365f707f791353962
Remington Rohel <rar129@usask.ca>
2022-09-16 14:48:24 +0000

    Fixed some PyCharm complaints with top-level __init__.py

--- backscatter/__init__.py
+++ backscatter/__init__.py
@@ -1,19 +1,16 @@
-import configparser as cp
-import os
 import backscatter.core.core as core
-
-#parsing config files
-config = core.parse_config_file()
-
-hdw_files_path = config.get("core","hdw_files_path")
-
-hdw_info = core.parse_hdw_files(hdw_files_path)
-
-#adding modules that will be seen at import                
-__all__ = ["fitacf","dmap"] 
-
 from .dmap import dmap
 from .fitacf import fitacf
+
+# parsing config files
+config = core.parse_config_file()
+hdw_files_path = config.get("core", "hdw_files_path")
+hdw_info = core.parse_hdw_files(hdw_files_path)
+
+# adding modules that will be seen at import
+__all__ = ["fitacf", "dmap"]
+
+
 
 
 
